59b6807fa47fff62bfdd11abbade73e2edb48c1d
Jaden Stock <jadenstock777@gmail.com>
2017-10-07 23:50:27 -0700

    event driven client almost working.

--- proj0/B/client.js
+++ proj0/B/client.js
@@ -6,45 +6,63 @@ var PORT = process.argv[3];
 
 // import dgram which offers support for UDP on node
 var dgram = require('dgram');
+var messages = require('../utils/messages');
 
 // make our server
 var client = dgram.createSocket('udp6');
 
-// set response types
-const HELLO = 0;
-const DATA = 1;
-const ALIVE = 2;
-const GOODBYE = 3;
+// generate a random session ID
+var sesID = Math.floor(Math.random() * (Math.pow(2, 32) - 1));
 
+// sequence number to keep track of correspondence
+var seqNum = 0;
+
+// after 30 seconds of no activity send GOODBYE and close
+var timeout = setTimeout(sendGoodbye, 3000000);
+
+// listen for messages from server
 client.on('listening', function () {
   var address = client.address();
   console.log('Listening on ' + address.address + ':'
     + address.port);
+
+  messages.sendMessage(client, PORT, HOST, seqNum, sesID, 0x0);
+
 });
 
-//client.on('message', function () {
-//})
+client.on('message', function () {
+  // now process the message
+  var pMessage = messages.processMessage(message);
+  var command = pMessage["command"];
 
-process.stdin.on('readable', function (data) {
-  console.log(data);
+  if (command == 0x0) { //
+  	clearTimeout(timeout);
+  	var timeout = setTimeout(sendGoodbye, 3000000);
+  } else if (command == 0x2) { //
+  	clearTimeout(timeout);
+  	var timeout = setTimeout(sendGoodbye, 3000000);
+  } else if (command == 0x3) { //
+  	client.close();
+  }
+
+})
+
+// on std input, send a data message to server
+process.stdin.on('data', function (data) {
+
+  messages.sendMessage(client, PORT, HOST, seqNum, sesID, 0x1, data);
+  seqNum++;
+  
+  clearTimeout(timeout);
+  var timeout = setTimeout(sendGoodbye, 30000);
+
 });
+
+//client.bind(69696);
 
 // Helper functions
+function sendGoodbye () {
+  console.log("sending goodbyes!");
+  messages.sendMessage(client, PORT, HOST, seqNum, sesID, 0x3);
 
-// sends messages of type HELLO, AlIVE, or GOODBYE
-function respond(session, type) {
-  var client = dgram.createSocket('udp6');
-
-  console.log("we're sending off a " + type);
-
-  var message = Buffer.allocUnsafe(12); // send 12 bytes of data for a header
-
-  message.writeUInt16BE(50273, 0); // magic
-  message.writeUInt8(1, 2); // version
-  message.writeUInt8(type, 3); // message type
-  message.writeUInt32BE(session["seqNum"], 4); // 
-  message.writeUInt32BE(session["sesID"], 8);
-
-  client.send(message, 0, message.length, session["clientPort"],
-    session["clientAddress"], function (err, bytes) { client.close(); });
 }
