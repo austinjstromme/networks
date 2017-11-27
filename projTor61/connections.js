bc6f1e2caaeb49aaff8cc1f9f0826df0ae354abb
Jaden Stock <jadenstock777@gmail.com>
2017-11-27 15:41:48 -0800

    a connections file.

---
+++ projTor61/connections.js
@@ -0,0 +1,16 @@
+var net = require('net');
+var cells = require('./cells');
+
+// object which holds TCP server connection which receives initiations from
+//    other routers. 
+exports.routerListener = function(router, port) {
+	var listener = net.createServer((socket) => {
+		var conn = new TCPRouterConnection(router, socket);
+	});
+}
+
+// An object containing a TCP connection between two routers. Handles incoming
+//	  and outgoing cells on this connection.
+exports.TCPRouterConnection = (router, socket) => {
+
+}
